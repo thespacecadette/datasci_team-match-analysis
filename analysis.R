041f8251ffd14fff7e89df18ee477f5258cef046
Tammy <tammy_nair@hotmail.com>
2022-05-27 18:37:22 +1000

    Pseudocode init

---
+++ analysis.R
@@ -0,0 +1,48 @@
+data <- read.csv("competitionResults.csv")
+
+# Total scored by each team for each match.
+# Each goal is worth 10 points, team that caught the snitch  awarded extra 50 points. 
+
+# TODO: [RESULTS] (table): 
+# * the season and round number
+# * the match number (a new variable that identies each match of the season)
+# * the home and away team names and their scores
+
+
+
+# The winner of each match has highest score. 
+# Each team scores 3 points for a win, 1 point for a draw 
+# Team with most points at the end of season is the season winner. 
+# TODO: [RESULT] Each team’s points as per the first 5 rounds of matches and full season. 
+# TODO: Season Results [TABLE] (two tables, one table for each set of results),
+# TODO: ordering teams by their points (i.e. a results ladder). 
+
+# Pick the season who’s last digit corresponds to 3.
+
+
+# An investigation into home game advantage is planned for the coming months. 
+# [RESULTS] Return: Count of 
+# * The number of home games each team has had
+# * The total number of games each team has played in
+# * The total points scored in a home game and the total number of points the team has scored. 
+# * Provide these results in a data frame. Run the function on the provided data and show the resulting table output.
+
+
+# Provide a table containing the team statistics for all seasons. 
+# Including, but not limited to the following:
+# * Number of games
+# * Number of wins
+# * Winning percentage, points scored, points conceded, no of snitch catches, goals scored, number of tournament wins. 
+# Teams ordered in terms of total points scored.
+
+
+
+
+
+
+
+
+
+
+
+
