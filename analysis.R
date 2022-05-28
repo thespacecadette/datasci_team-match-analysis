f3c1752e00b02d547f1c3c199b0ae681c557b36d
Tammy <tammy_nair@hotmail.com>
2022-05-28 13:50:18 +1000

    Get total points for each team (including snitch points)

--- analysis.R
+++ analysis.R
@@ -1,17 +1,33 @@
-data <- read.csv("competitionResults.csv")
+df <- read.csv("competitionResults.csv")
 
-# Total scored by each team for each match.
-# Each goal is worth 10 points, team that caught the snitch  awarded extra 50 points. 
+# DATA PREPROCESSING
+head(df)
 
-# TODO: [RESULTS] (table): 
+# [RESULT] Total scored by each team for each match. Each goal is worth 10 points, team that caught the snitch  awarded extra 50 points. 
 # * the season and round number
-# * the match number (a new variable that identies each match of the season)
+# * the match number (a new variable that identifies each match of the season)
 # * the home and away team names and their scores
+seasonTable <- data.frame(matrix(ncol=7, nrow=0))
+headings <- c("season", "round", "matchNo", "homeTeam", "homeTeamPoints", "awayTeam", "awayTeamPoints")
+colnames(seasonTable) <- headings
+print(seasonTable)
 
 
+for (match_number in 1:nrow(df)) {
+  match <- df[match_number,]
+  totalPointsHomeTeam <- match[["homeGoals"]] * 10 + ifelse(match[["homeTeam"]] == match[["snitch"]], 50, 0)
+  totalPointsAwayTeam <- match[["awayGoals"]] * 10 + ifelse(match[["awayTeam"]] == match[["snitch"]], 50, 0)
 
-# The winner of each match has highest score. 
-# Each team scores 3 points for a win, 1 point for a draw 
+  seasonTable[match_number, 1] <- match[["Season"]]
+  seasonTable[match_number, 2] <- match[["round"]]
+  seasonTable[match_number, 3] <- match_number
+  seasonTable[match_number, 4] <- match[["homeTeam"]]
+  seasonTable[match_number, 5] <- totalPointsHomeTeam
+  seasonTable[match_number, 6] <- match[["awayTeam"]]
+  seasonTable[match_number, 7] <- totalPointsAwayTeam
+}
+
+head(seasonTable)
 # Team with most points at the end of season is the season winner. 
 # TODO: [RESULT] Each team’s points as per the first 5 rounds of matches and full season. 
 # TODO: Season Results [TABLE] (two tables, one table for each set of results),
